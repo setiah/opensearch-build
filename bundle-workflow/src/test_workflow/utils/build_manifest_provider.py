f201f417a7b1b6bb8723245a3a2283f5f935ebd4
Himanshu Setia <setiah@amazon.com>
2021-09-10 07:30:13 -0700

    Adding README and minor fixes
    Signed-off-by: Himanshu Setia <setiah@amazon.com>

--- bundle-workflow/src/test_workflow/utils/build_manifest_provider.py
+++ bundle-workflow/src/test_workflow/utils/build_manifest_provider.py
@@ -8,7 +8,7 @@
 # GitHub history for details.
 
 import os
-from manifests.bundle_manifest import BundleManifest
+from manifests.build_manifest import BuildManifest
 from aws.s3_bucket import S3Bucket
 
 
@@ -23,4 +23,4 @@ class BuildManifestProvider:
         manifest_s3_path = BuildManifestProvider.get_build_manifest_relative_location(build_id, opensearch_version, architecture)
         S3Bucket(bucket_name).download_file(manifest_s3_path, local_path)
         with open('manifest.yml', 'r') as file:
-            return BundleManifest.from_file(file)
+            return BuildManifest.from_file(file)
